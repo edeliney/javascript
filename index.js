db2203f9a521b5fe80b5ca3d743d9f068cf409e6
Edeline <edeline96@hotmail.com>
2021-01-13 17:02:24 +0900

    Used Variable

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello Edeline')
+console.log('Hello Edeline');
+// console.log(3000*2);
+//state
+//let a;
+//a = 3000;
+let a = 3000;
+let b = 4000;
+let c = 5000;
+
+console.log(a * 2);
+console.log(b * 2 + c * 2);
+console.log(c * 2 + a);
+
+
