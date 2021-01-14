31f160beb286fb1a93e7e04f49fab4bf5a52ae45
Edeline <edeline96@hotmail.com>
2021-01-14 12:05:32 +0900

    Add let practice

--- index.js
+++ index.js
@@ -11,4 +11,19 @@ console.log(a * 2);
 console.log(b * 2 + c * 2);
 console.log(c * 2 + a);
 
+// let practice
+
+let coffee = 10;
+let milk = 150;
+let oatmilk = 160;
+let syrup = 30;
+let whippedCream = 70;
+
+// test
+console.log(coffee);
+console.log(coffee + milk); 
+console.log(coffee + oatmilk); 
+console.log(coffee + milk + syrup);
+console.log(coffee + milk + syrup + whippedCream; // 모카(휘핑 추가) 칼로리
+
 
