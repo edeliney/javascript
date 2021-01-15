c902a233059024baee5c4a03ac70dbf0cd2c5d7e
Edeline <edeline96@hotmail.com>
2021-01-15 13:04:54 +0900

    Learned about numbers using javascript

---
+++ morejava.js
@@ -0,0 +1,22 @@
+// 1. 숫자형
+// 거듭제곱
+console.log(3 ** 3);
+
+// 나머지
+console.log(7 % 3);
+
+// 몫
+const i = Math.trunc(7 / 3);
+console.log(typeof i, i);
+
+// 덧셈
+console.log(2 + 8);
+
+// 뺄셈
+console.log(4 - 5);
+
+// 곱셈
+console.log(4 * 5);
+
+// 나눗셈
+console.log(10 / 3);
