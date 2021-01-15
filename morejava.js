ba98ec965531febc80a26a77f3ea97da7aae4f41
Edeline <edeline96@hotmail.com>
2021-01-15 13:33:27 +0900

    Add Boolean examples

--- morejava.js
+++ morejava.js
@@ -38,3 +38,45 @@ console.log("My mother said \"You should not trust boys.\" and I believe her.\""
 console.log('The popular line I think in \'Harry Potter\' is \"Shut up, Malfoy\".');
 console.log("The popular line I think in \'Harry Potter\' is \"Shut up, Malfoy\".");
 console.log("The popular line I think in 'Harry Potter' is \"Shut up, Malfoy\".");
+
+
+// 3. Boolean
+console.log(3 > 2);
+console.log(3 < 5);
+console.log(5 >= 1);
+console.log(6 <= 9);
+console.log(2 === 2);
+
+
+//AND &&
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+//OR ||
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+//NOT !
+console.log(!true);
+console.log(!false);
+console.log(!!true);
+console.log(!!false);
+
+// Mix
+console.log(3 < 1 && 'Me' !== 'Me');
+console.log(23 !== 23 || 7 < 9);
+
+let i = 9
+console.log(i > 23 || !(i > 8));
+
+//console.log(i > 23 || !(i > 8));
+//console.log(9 > 23 || !(i > 8));
+//console.log(false || !(i > 8));
+//console.log(false || !(9 > 8));
+//console.log(false || !true);
+//console.log(false || false);
+// = false
